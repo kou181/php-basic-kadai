3364e3b0498bd8c1b1071bf9789ce0b59831baee
kou181 <regard_fm@yahoo.co.jp>
2024-10-09 11:26:18 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <body>
+        <?php
+        $personal_data=['name'=>'onion','price'=>200,'weight'=>160];
+        print_r($personal_data);
+        ?>
+    </body>
+</html>
