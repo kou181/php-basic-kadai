147176e53b6c721045e0270a1d800bf86ea0b6f0
kou181 <regard_fm@yahoo.co.jp>
2024-10-09 11:14:54 +0900

    Add files via upload

---
+++ kadai_005/kadai_005/score.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+    </head>
+    <body>
+        <?php
+        $score0=80;
+        $score1=60;
+        $score2=55;
+        $score3=40;
+        $score4=100;
+        $score5=25;
+        $score6=80;
+        $score7=95;
+        $score8=30;
+        $score9=60;
+
+        $total=$score0+$score1+$score2+$score3+$score4+$score5+$score6+$score7+$score8+$score9;
+        $average=$total / 10;
+
+        echo $average ;
+
+?>
+    </body>
+</html>
